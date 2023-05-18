d0c41fd7a36ea862932d2d89adde2d0383278395
PerkGuacamole <evanunmann@gmail.com>
2023-05-17 22:25:02 -0500

    Removing dead code from build.zig

--- build.zig
+++ build.zig
@@ -1,29 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
-    var sources = std.ArrayList([]const u8).init(b.allocator);
-
-    // Search for all C/C++ files in `src` and add them
-    {
-        var dir = try std.fs.cwd().openIterableDir("src", .{});
-
-        var walker = try dir.walk(b.allocator);
-        defer walker.deinit();
-
-        const allowed_exts = [_][]const u8{ ".c", ".cpp", ".cxx", ".c++", ".cc" };
-        while (try walker.next()) |entry| {
-            const ext = std.fs.path.extension(entry.basename);
-            const include_file = for (allowed_exts) |e| {
-                if (std.mem.eql(u8, ext, e))
-                    break true;
-            } else false;
-            if (include_file) {
-                // we have to clone the path as walker.next() or walker.deinit() will override/kill it
-                try sources.append(entry.path);
-            }
-        }
-    }
-
     const chess_engine_exe = b.addExecutable(.{ .name = "chess_engine" });
     chess_engine_exe.addCSourceFiles(&.{
         "./src/BitBoard.cpp",
@@ -56,7 +33,7 @@ pub fn build(b: *std.build.Builder) !void {
         "./src/Timer.cpp",
         "./src/UCIUtils.cpp",
         "./src/ZobristHash.cpp",
-    }, &.{ "-std=c++2b", "-W", "-Wall", "-fexperimental-library", "-stdlib=libc++" });
+    }, &.{ "-std=c++2b", "-W", "-Wall", "-fexperimental-library", "-stdlib=libc++", "-O3" });
     chess_engine_exe.linkLibCpp();
     b.installArtifact(chess_engine_exe);
 
@@ -102,7 +79,7 @@ pub fn build(b: *std.build.Builder) !void {
         "./test/ProcessMoveTests.cpp",
         "./test/ShiftTests.cpp",
         "./test/Tests.cpp",
-    }, &.{ "-std=c++2b", "-W", "-Wall", "-fexperimental-library", "-stdlib=libc++" });
+    }, &.{ "-std=c++2b", "-W", "-Wall", "-fexperimental-library", "-stdlib=libc++", "-O3" });
     chess_engine_tests_exe.linkLibCpp();
     b.installArtifact(chess_engine_tests_exe);
 }
